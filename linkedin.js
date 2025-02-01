b2f95483a4af676198439bd417c592a788ccd4b3
Dede22dev <deyvidpasamaier16@gmail.com>
2025-02-01 17:27:12 -0300

    Update linkedin.js
    Havia esquecido de por o meu perfil, tinha deixado apenas o material de teste.

--- linkedin.js
+++ linkedin.js
@@ -1,4 +1,4 @@
 const button = document.getElementById("abrirlinkedin");
 button.addEventListener("click", function () {
-  window.open("https://www.linkedin.com/in/seu-perfil", "_blank");
+  window.open("https://www.linkedin.com/in/deyvid-pasa-maier-308329276/", "_blank");
 });
